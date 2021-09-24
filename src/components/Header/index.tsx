84fa314e7f6a41ed8cf84de4f846cab6cd3d0079
Maurício de Sousa Carvalho <mauricio.carvalho@radixeng.com.br>
2021-09-24 16:57:10 -0300

    adjusting the yellow tag below the selected route

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
-import styles from './styles.module.scss';
 import { SignInButton } from '../SignInButton/index';
+import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink/index';
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
 import Head from 'next/head';
 import styles from './styles.module.scss';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,26 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time> 12 de março de 2021</time>
-            <strong>Creating a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a Monorepo</p>
-          </a>
-          <a href="#">
-            <time> 12 de março de 2021</time>
-            <strong>Creating a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a Monorepo</p>
-          </a>
-          <a href="#">
-            <time> 12 de março de 2021</time>
-            <strong>Creating a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a Monorepo</p>
-          </a>
-          <a href="#">
-            <time> 12 de março de 2021</time>
-            <strong>Creating a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a Monorepo</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -49,9 +48,20 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: { posts }
   }
 }
